580df245b5b1b5ef74b5283a164e72465550067e
Tan Thanh <thanh.do@mati.com.vn>
2026-08-24 00:45:57 +0700

    Revert "feat: implement environment variable validation with @nestjs/config and Joi"
    This reverts commit e03b14b4f1251747edea8adaa5a9a72d9d592a18.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: envValidationSchema,
-    }),
-    UsersModule,
-  ],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
--- src/config/env.validation.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from 'joi';
-
-export const envValidationSchema = Joi.object({
-  NODE_ENV: Joi.string()
-    .valid('development', 'preprod', 'prod')
-    .default('development'),
-  PORT: Joi.number().default(3000),
-  DATABASE_URL: Joi.string().required(),
-});
