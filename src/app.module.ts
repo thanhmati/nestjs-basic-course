e03b14b4f1251747edea8adaa5a9a72d9d592a18
Tan Thanh <thanh.do@mati.com.vn>
2026-08-23 16:42:02 +0700

    feat: implement environment variable validation with @nestjs/config and Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: envValidationSchema,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
