9cb9654d0913c0f5752bed98985bf80f903213e1
thanhmati <thanh.do@mati.com.vn>
2026-08-26 15:22:11 +0700

    feat: implement global API versioning and update post endpoints to support multi-version routing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT', 3000);
+  const globalPrefix = configService.get<string>('GLOBAL_PREFIX');
+  const versionPrefix = configService.get<string>('VERSION_PREFIX');
+  const versionApi = configService.get<string>('VERSION_API');
+
+  app.setGlobalPrefix(globalPrefix || 'api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: versionPrefix,
+    defaultVersion: versionApi,
+  });
+
+  await app.listen(port);
+  console.log(
+    `Application is running on: http://localhost:${port}/${globalPrefix}/${versionPrefix}${versionApi}`,
+  );
 }
-bootstrap();
+void bootstrap();
